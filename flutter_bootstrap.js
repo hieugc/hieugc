1c0f778679dae5b81ede2d5fce192b94ea7aab25
hieupham <hieupham@safeforweb.com>
2026-01-23 02:32:49 +0700

    Deploy: Hide copyright, update resume URL

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2709572420"
+    serviceWorkerVersion: "893757305"
   }
 });
